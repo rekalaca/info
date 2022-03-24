6a87ca593cfa15781e45d93defa585db76234822
rekalaca <rekalaca@gmail.com>
2022-03-24 11:25:30 +0100

    Validation

--- src/components/Regist.js
+++ src/components/Regist.js
@@ -7,8 +7,15 @@ export default function Regist() {
 
 
     return (
+        
 
         <div className='regist'>
+            <p className='regduma'>Termék vagy szolgáltatás megrendeléshez szükség lesz regisztrációra, amit az alábbi felületen tudsz megtenni! 
+                Cég regisztrálásánál természetesen a születési dátumot nem kell kitölteni, viszont az adószámot igen! 
+                Magánszemély esetén pedig egyértelmű, hogy az adószám elhagyható, a születési dátum kitöltése viszont kötelező elem. 
+                Amennyiben a szállítást, vagy a megrendelt munkát másik címre kéri, úgy szükséges a jelölő négyzet bepipálása, majd 
+                a másik cím megadása is. Felhívjuk figyelmedet, hogy cégünk egyelőre Szabolcs Szatmár – Bereg megye területén vállal 
+                kiszállást és helyszíni munkát!</p>
             <Form />
         </div>
 
--- src/components/subpage/Form.js
+++ src/components/subpage/Form.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useState } from "react";
-import Adat from "./regist.json";
+import Adat from "./users.json";
 import 'bootstrap/dist/css/bootstrap.css';
-import Form from 'react-bootstrap/Form';
 
 export default function App() {
 
@@ -15,141 +14,122 @@ export default function App() {
     const [password2, setPassword2] = useState('');
     const [goodpassword, setgoodPassword] = useState(true);
     const [birth, setBirth] = useState('');
+    const [goodbirth, setgoodBirth] = useState(true);
+    const [email, setEmail] = useState('');
+    const [goodemail, setgoodEmail] = useState(true);
+    const [phone, setPhone] = useState('');
+    const [goodphone, setgoodPhone] = useState(true);
+    const [tax_reg, setTax_reg] = useState('');
+    const [goodtax_reg, setgoodTax_reg] = useState(true);
+    const [billing_address, setBilling_address] = useState('');
+    const [goodbilling_address, setgoodBilling_address] = useState(true);
+    const [shipping_address, setShipping_address] = useState('');
+    const [goodshipping_address, setgoodShipping_address] = useState(true);
 
     const send = () => {
-        setAdat(Adat.push({ name: name, login:login, password:password, birth:birth }));
-        console.log(Adat);  
+        setAdat(Adat.push({ login: login, password: password, name: name, phone: phone, birth: birth, email: email, billing_address: billing_address, shipping_address: shipping_address, tax_reg: tax_reg }));
+        console.log(Adat);
     }
 
 
     function vizsgal() {
 
-        if (name.length >= 3 && name.match(" ")) setgoodName(true)
-        else setgoodName(false);
-
-        if (login.length > 6 && login.length < 20 && !login.match(" ") && login == login.toLowerCase()) setgoodLogin(true)
+        if (login.length > 5 && login.length < 21 && !login.match(" ") && login == login.toLowerCase()) setgoodLogin(true)
         else setgoodLogin(false);
 
-        if (password.length > 7 && password.length < 17 && password == password2) setgoodPassword(true)
+        var passformat =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,16}$/;
+        if (password.length > 7 && password.length < 17 && password == password2 && password.match(passformat)) setgoodPassword(true)
         else setgoodPassword(false);
 
-        return send();        
+        var nameformat =/^[A-ZÍÁÉŰÚŐÓ][a-zíéáűúőó]*[ ]{1}[A-ZÍÁÉŰÚŐÓ][a-zíéáűúőó\D]*$/;
+        if (name.length > 0 && name.match(nameformat)) setgoodName(true)
+        else setgoodName(false);
 
+        if (birth.length > 7) setgoodBirth(true)
+        else setgoodBirth(false);
 
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (email.match(mailformat)) setgoodEmail(true)
+        else setgoodEmail(false);
+
+        if (phone.length > 7 && phone.length < 10) setgoodPhone(true)
+        else setgoodPhone(false);
+
+        var taxformat = (/^[0-9]{8}[-]{1}[0-9]{1}[-]{1}[0-9]{2}$/); /* EZT JAVÍTANI KELL!*/
+        if (tax_reg.length == 0 || tax_reg.match(taxformat)) setgoodTax_reg(true)
+        else setgoodTax_reg(false);
+
+        var addressformat = /^([0-9]{4}[ ]{1}[A-ZÍÁÉŰÚŐÓ][a-zíéáűúőó]*[,]{1}[ ]{1}[A-ZÍÁÉŰÚŐÓ][a-zíéáűúőó]*[ ]{1}[\wa-zíéáűúőó]*[ ]{1}[\w][a-z íéáűúőó\./\-0-9]*)$/;
+        if (billing_address.match(addressformat)) setgoodBilling_address(true)
+        else setgoodBilling_address(false);
+
+        var addressformat2 = /^([0-9]{4}[ ]{1}[A-ZÍÁÉŰÚŐÓ][a-zíéáűúőó]*[,]{1}[ ]{1}[A-ZÍÁÉŰÚŐÓ][a-zíéáűúőó]*[ ]{1}[\w][a-zíéáűúőó]*[ ]{1}[\w][a-z íéáűúőó\./\-0-9]*)$/;
+        if (shipping_address.length == 0 || shipping_address.match(addressformat2)) setgoodShipping_address(true)
+        else setgoodShipping_address(false);
+           
+            
+             
 
     }
 
     return (
         <>
-            <h4>Regisztráció</h4>
+            <div id='regcim'><h3>Regisztráció</h3></div>
 
-            <div className="col-md-6">
-                <label for="formGroupExampleInput" class="form-label">Név</label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="név vagy cégnév" onChange={(e) => setName(e.target.value)} /><label className='hibauzi'>{goodname ? '' : 'Hibás név formátum!'}</label>
-            </div>
-            <div className="col-md-6">
+            
+            <div className="col-md-8 menusk">
                 <label for="formGroupExampleInput" class="form-label">Felhasználónév</label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="csupa kisbetű-ékezet nélkül-szóköz nélkül" onChange={(e) => setLogin(e.target.value)} /><label className='hibauzi'>{goodlogin ? '' : 'Hibás felhasználónév formátum! (csak kisbetű, minimum 6, maximum 20 karakter, szóköz nélkül)'}</label>
+                <input type="text" className="form-control" id="login" placeholder="csupa kisbetű ékezet nélkül szóköz nélkül (min 6 - max 20 karakter)" onChange={(e) => setLogin(e.target.value)} /><label className='hibauzi'>{goodlogin ? '' : 'Hibás felhasználónév formátum! (csak kisbetű, minimum 6, maximum 20 karakter, szóköz nélkül)'}</label>
+            </div>            
+            <div className="col-md-8 menusk">
+                <label for="inputPassword" className="form-label">Jelszó</label>
+                <input type="password" className="form-control" id="password" placeholder="minimum 8, maximum 16 karakter(kis/nagybetű/szám+speciális karakter)" onChange={(e) => setPassword(e.target.value)} /><label className='hibauzi'>{goodpassword ? '' : 'Hibás jelszó formátum, vagy nem egyezik a két jelszó!'}</label>
             </div>
-            <div className="col-md-6">
-                <label for="inputPassword1" className="form-label">Jelszó</label>
-                <input type="password" className="form-control" id="inputPassword4" placeholder="minimum 8, maximum 16 karakter(kis/nagybetű és szám)" onChange={(e) => setPassword(e.target.value)}/><label className='hibauzi'>{goodpassword ? '' : 'Hibás jelszó formátum, vagy nem egyezik a két jelszó!'}</label>
+            <div className="col-md-8 menusk">
+                <label for="inputPassword" className="form-label">Jelszó mégegyszer</label>
+                <input type="password" className="form-control" id="password22" placeholder="minimum 8, maximum 16 karakter(kis/nagybetű/szám+speciális karakter)" onChange={(e) => setPassword2(e.target.value)} /><label className='hibauzi'>{goodpassword ? '' : 'Hibás jelszó formátum, vagy nem egyezik a két jelszó!'}</label>
             </div>
-            <div className="col-md-6">
-                <label for="inputPassword2" className="form-label">Jelszó mégegyszer</label>
-                <input type="password" className="form-control" id="inputPassword4" placeholder="minimum 8, maximum 16 karakter(kis/nagybetű és szám)" onChange={(e) => setPassword2(e.target.value)}/><label className='hibauzi'>{goodpassword ? '' : 'Hibás jelszó formátum, vagy nem egyezik a két jelszó!'}</label>
+            <div className="col-md-8 menusk">
+                <label for="formGroupExampleInput" class="form-label">Név:</label>
+                <input type="text" className="form-control" id="name" placeholder="" onChange={(e) => setName(e.target.value)} /><label className='hibauzi'>{goodname ? '' : 'Hibás név formátum!'}</label>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-8 menusk">
                 <label for="date" className="form-label">Születési dátum (magánszemély esetében):</label>
-                <input type="date" className="form-control" id="date" placeholder="" onChange={(e) => setBirth(e.target.value)}/>
+                <input type="date" className="form-control" id="birth" placeholder="" onChange={(e) => setBirth(e.target.value)} /><label className='hibauzi'>{goodbirth ? '' : 'Hibás dátum!'}</label>
             </div>
-
-
-            <Form>                
-                
-                <Form.Group>
-                    <Form.Label>Email cím:</Form.Label>
-                    <Form.Control type="email"
-                        placeholder="" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Telefonszám:</Form.Label>
-                    <Form.Control type="number"
-                        placeholder="pl:301234567" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Adószám (cég esetében):</Form.Label>
-                    <Form.Control type="number"
-                        placeholder="" />
-                </Form.Group>
-                <h4>Számlázási cím:</h4>
-                <Form.Group>
-                    <Form.Label>Irányítószám:</Form.Label>
-                    <Form.Control type="number"
-                        placeholder="" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Város:</Form.Label>
-                    <Form.Control type="text"
-                        placeholder="" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Utca:</Form.Label>
-                    <Form.Control type="text"
-                        placeholder="" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Házszám:</Form.Label>
-                    <Form.Control type="number"
-                        placeholder="" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>További cím adat:</Form.Label>
-                    <Form.Control type="text"
-                        placeholder="épület, házbetű, emelet, ajtó" />
-                </Form.Group>
-
-                <div className="shiping-details">
-                    <div className="input-checkbox">
-                        <input type="checkbox" id="shiping-address" />
-                        <label for="shiping-address">
-                            <span></span>
-                            A szállítást más címre kérem!
-                        </label>
-                        <div className="caption">
-                            <h4>Szállítási cím:</h4>
-                            <Form.Group>
-                                <Form.Label>Irányítószám:</Form.Label>
-                                <Form.Control type="number"
-                                    placeholder="" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Város:</Form.Label>
-                                <Form.Control type="text"
-                                    placeholder="" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Utca:</Form.Label>
-                                <Form.Control type="text"
-                                    placeholder="" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Házszám:</Form.Label>
-                                <Form.Control type="number"
-                                    placeholder="" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>További cím adat:</Form.Label>
-                                <Form.Control type="text"
-                                    placeholder="épület, házbetű, emelet, ajtó" />
-                            </Form.Group>
+            <div className="col-md-8 menusk">
+                <label for="formGroupExampleInput" class="form-label">Email:</label>
+                <input type="text" className="form-control" id="email" placeholder="" onChange={(e) => setEmail(e.target.value)} /><label className='hibauzi'>{goodemail ? '' : 'Hibás email formátum!'}</label>
+            </div>
+            <div className="col-md-8 menusk">
+                <label for="formGroupExampleInput" class="form-label">Telefonszám:</label>
+                <input type="number" className="form-control" id="phone" placeholder="pl: 301234567" onChange={(e) => setPhone(e.target.value)} /><label className='hibauzi'>{goodphone ? '' : 'Hibás telefonszám formátum!'}</label>
+            </div>
+            <div className="col-md-8 menusk">
+                <label for="formGroupExampleInput" class="form-label">Adószám:</label>
+                <input type="text" className="form-control" id="tax_reg" placeholder="12345678-9-10" onChange={(e) => setTax_reg(e.target.value)} /><label className='hibauzi'>{goodtax_reg ? '' : 'Hibás adószám formátum!'}</label>
+            </div>
+            <div className="col-md-8 menusk">
+                <label for="formGroupExampleInput" class="form-label">Számlázási cím:</label>
+                <input type="text" className="form-control" id="billing_address" placeholder="4400 Nyíregyháza, Városmajor utca 4." onChange={(e) => setBilling_address(e.target.value)} /><label className='hibauzi'>{goodbilling_address ? '' : 'Hibás cím formátum!'}</label>
+            </div>
+            <div className="shiping-details kozep">
+                <div className="input-checkbox">
+                    <input type="checkbox" id="shiping-address" />
+                    <label for="shiping-address">
+                    <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+                        A szállítást más címre kérem!
+                    </label>
+                    <div className="caption">
+                        <div className="col-md-8 menusk">
+                            <label for="formGroupExampleInput" class="form-label">Szállítási cím:</label>
+                            <input type="text" className="form-control" id="shipping_address" placeholder="" onChange={(e) => setShipping_address(e.target.value)} /><label className='hibauzi'>{goodshipping_address ? '' : 'Hibás cím formátum!'}</label>
                         </div>
                     </div>
                 </div>
+            </div>
 
-
-            </Form>
-            <button class="btn btn-primary gomb" onClick={() => vizsgal()}>Regisztrálok!</button>
+            <div className='kozep'><button class="btn btn-primary gomb" onClick={() => vizsgal()}>Regisztrálok!</button></div>
 
         </>
     );
