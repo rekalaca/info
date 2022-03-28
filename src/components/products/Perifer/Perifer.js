b76e6b9564bd34c083d376c91ff7a2e8a8d30777
baloghbalint888 <baloghbalint888@gmail.com>
2022-03-28 14:30:08 +0200

    served with back-end

--- src/components/products/Perifer/Perifer.js
+++ src/components/products/Perifer/Perifer.js
@@ -1,65 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Button } from 'react-bootstrap';
-import hpdesk1 from './img/HPD2320/hpdesk01.jpg';
-import pixma1 from './img/Canon/pixma01.png';
-import hpnk1 from './img/HPnk/hpnk01.jpg';
-import ep1 from './img/Epson/ep01.jpg';
+import axios from 'axios';
 
 export default function Perifer() {
 
-    return(
 
+
+    const [data, setData] = useState([]);
+    const fetchdata = async () => {
+		const result = await axios.get("http://localhost:5555/productsPeri")
+		setData(result.data)
+	}
+
+    useEffect(() => {
+		fetchdata();
+		
+	},[]);
+    console.log(data)
+    return (
         <div className='section'>
             <div className='container'>
                 <div className="row">
-                    <div className='col-md-3 col-xs-6'>
-                        <Card style={{ width: '100%' }}>
-                            <Card.Img variant="top" src={hpdesk1} />
-                            <Card.Body>
-                                <Card.Title>HP Deskjet 2320 All-in-One Tintasugaras Nyomtató</Card.Title>
-                                <Card.Text>
-                                    Többfunkciós nyomtató - Tintasugaras - Színes - Otthon / Otthoni iroda - Nyomtatás - Szkennelés - Másolás - Fehér színben
-                                </Card.Text>
-                                <Button className='gomb' variant="primary">Részletek</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className='col-md-3 col-xs-6'>
-                        <Card style={{ width: '100%' }}>
-                            <Card.Img variant="top" src={pixma1} />
-                            <Card.Body>
-                                <Card.Title>Canon PIXMA TS3450 multifunkciós tintasugaras</Card.Title>
-                                <Card.Text>
-                                Fényképnyomtató - Többfunkciós nyomtató - Tintasugaras - Monokróm, Színes - Otthon / Otthoni iroda - Nyomtatás - Szkennelés
-                                </Card.Text>
-                                <Button className='gomb' variant="primary">Részletek</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className='col-md-3 col-xs-6'>
-                        <Card style={{ width: '100%' }}>
-                            <Card.Img variant="top" src={hpnk1} />
-                            <Card.Body>
-                                <Card.Title>HP Deskjet Ink Advantage 3788 Multifunkciós</Card.Title>
-                                <Card.Text>
-                                Többfunkciós nyomtató - Tintasugaras - Színes - Otthon / Otthoni iroda - Nyomtatás - Szkennelés - Másolás - Fax - USB/WiFi
-                                </Card.Text>
-                                <Button className='gomb' variant="primary">Részletek</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className='col-md-3 col-xs-6'>
-                        <Card style={{ width: '100%' }}>
-                            <Card.Img variant="top" src={ep1} />
-                            <Card.Body>
-                                <Card.Title>Epson L3210 multifunkciós tintatartályos nyomtató</Card.Title>
-                                <Card.Text>
-                                Többfunkciós nyomtató - Tintasugaras - Színes - Otthon / Otthoni iroda - Nyomtatás - Szkennelés - Másolás - Fekete színben
-                                </Card.Text>
-                                <Button className='gomb' variant="primary">Részletek</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
+                {data.map((elem,index)=>(
+                    <div key={index} className='col-md-3 col-xs-6'>
+                    <Card style={{ width: '100%' }}>
+                        <Card.Img variant="top" src={elem.picture} />
+                        <Card.Body>
+                            <Card.Title>{elem.name}</Card.Title>
+                            <Card.Text>
+                                {elem.description}
+                            </Card.Text>
+                            <Card.Text>
+                                {elem.net_value}
+                            </Card.Text>
+                            <Button className='gomb' variant="primary">Részletek</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                ))}
+
                 </div>
             </div>
         </div >
--- src/components/subpage/Slider.js
+++ src/components/subpage/Slider.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import axios from 'axios';
 import p1 from './img/product01.png';
 import p2 from './img/product02.png';
 import p3 from './img/product03.png';
@@ -9,6 +11,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 export default function Sliderjs() {
     const settings = {
+
         dots: true,
         infinite: true,
         speed: 500,
@@ -41,6 +44,20 @@ export default function Sliderjs() {
             }
         ]
     };
+
+    const [data, setData] = useState([]);
+    const fetchdata = async () => {
+		const result = await axios.get("http://localhost:5555/products")
+		setData(result.data)
+	}
+
+    useEffect(() => {
+		fetchdata();
+		
+	},[]);
+    console.log(data)
+
+
     return (
         <div className="section">
             <div className="container">
@@ -48,11 +65,11 @@ export default function Sliderjs() {
                 <div className="row">
 
                     <div className="col-md-12">
-                        <div className="row">
+
                             <div className="products-tabs">
                                 <Slider {...settings} >
-
-                                    <div className="product">
+                                    {data.map((elem,index)=>(
+                                        <div className="product" key={index}>
                                         <div className="product-img">
                                             <img src={p1} alt="" />
                                             <div className="product-label">
@@ -62,85 +79,16 @@ export default function Sliderjs() {
                                         </div>
                                         <div className="product-body">
                                             <p className="product-category">Laptop</p>
-                                            <h3 className="product-name"><a href="#">LENOVO V15-IIL (82C5000QHV) Laptop</a></h3>
-                                            <h4 className="product-price">279.000 Ft <del className="product-old-price">299.000 Ft</del></h4>
+                                            <h3 className="product-name"><a href="#">{elem.name}</a></h3>
+                                            <h4 className="product-price">279.000 Ft <del className="product-old-price">{elem.net_value} Ft</del></h4>
                                         </div>
                                         <div className="add-to-cart">
                                             <button className="add-to-cart-btn">kosárba rakom</button>
                                         </div>
                                     </div>
-
-
-
-                                    <div className="product">
-                                        <div className="product-img">
-                                            <img src={p2} alt="" />
-                                            <div className="product-label">
-                                                <span className="new">ÚJ!</span>
-                                            </div>
-                                        </div>
-                                        <div className="product-body">
-                                            <p className="product-category">WiFi router</p>
-                                            <h3 className="product-name"><a href="#">TP-Link AX1500 Next-Gen WiFi router</a></h3>
-                                            <h4 className="product-price">8.990 Ft <del className="product-old-price">9.990 Ft</del></h4>
-                                        </div>
-                                        <div className="add-to-cart">
-                                            <button className="add-to-cart-btn">kosárba rakom</button>
-                                        </div>
-                                    </div>
-
-
-                                    <div className="product">
-                                        <div className="product-img">
-                                            <img src={p3} alt="" />
-                                            <div className="product-label">
-                                                <span className="new">ÚJ!</span>
-                                            </div>
-                                        </div>
-                                        <div className="product-body">
-                                            <p className="product-category">Laptop</p>
-                                            <h3 className="product-name"><a href="#">LENOVO IDEAPAD 3 15IIL05 Laptop + Windows 10</a></h3>
-                                            <h4 className="product-price">179.000 Ft <del className="product-old-price">199.000 Ft</del></h4>
-                                        </div>
-                                        <div className="add-to-cart">
-                                            <button className="add-to-cart-btn">kosárba rakom</button>
-                                        </div>
-                                    </div>
-
-                                    <div className="product">
-                                        <div className="product-img">
-                                            <img src={p4} alt="" />
-                                            <div className="product-label">
-                                                <span className="new">ÚJ!</span>
-                                            </div>
-                                        </div>
-                                        <div className="product-body">
-                                            <p className="product-category">Periféria</p>
-                                            <h3 className="product-name"><a href="#">HP DESKJET 2320 ALL-IN-ONE nyomtató</a></h3>
-                                            <h4 className="product-price">14.990 Ft <del className="product-old-price">17.990 Ft</del></h4>
-                                        </div>
-                                        <div className="add-to-cart">
-                                            <button className="add-to-cart-btn">kosárba rakom</button>
-                                        </div>
-                                    </div>
-
-
-                                    <div className="product">
-                                        <div className="product-img">
-                                            <img src={p5} alt="" />
-                                        </div>
-                                        <div className="product-body">
-                                            <p className="product-category">Kábel</p>
-                                            <h3 className="product-name"><a href="#">ROLINE UTP CAT6 PATCH KÁBEL 1 méteres</a></h3>
-                                            <h4 className="product-price">790 Ft <del className="product-old-price">990 Ft</del></h4>
-                                        </div>
-                                        <div className="add-to-cart">
-                                            <button className="add-to-cart-btn">kosárba rakom</button>
-                                        </div>
-                                    </div>
+                                    ))}
                                 </Slider>
                             </div>
-                        </div>
                     </div>
 
                 </div>
